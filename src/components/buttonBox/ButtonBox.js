f903c72216ebbb82419f0e4a8905b91cb642b006
Steven <stevengf14@gmail.com>
2024-04-03 13:25:26 -0500

    buttons and actions

--- src/components/buttonBox/ButtonBox.js
+++ src/components/buttonBox/ButtonBox.js
@@ -1,15 +1,23 @@
+import { connect } from "react-redux";
 import "./ButtonBox.css";
+import navState from "../../redux/reducers/navReducer";
+import navClick from "../../redux/actions/navActions";
 
-const ButtonBox = () => {
+const ButtonBox = (_title) => {
+  const buttonClick = () => {
+    navClick({
+      title: _title,
+    });
+  };
   return (
     <div className="ButtonBox">
       <header className="App-header">
         <div>
           <h1>Title</h1>
           <ul>
-            <li>Home</li>
-            <li>List</li>
-            <li>New</li>
+            <li onClick={() => buttonClick("Home")}>Home</li>
+            <li onClick={() => buttonClick("List")}>List</li>
+            <li onClick={() => buttonClick("New")}>New</li>
           </ul>
         </div>
       </header>
@@ -17,4 +25,9 @@ const ButtonBox = () => {
   );
 };
 
-export default ButtonBox;
+const mapStateToProps = (state) => {
+  return {
+    title: state.navState.title,
+  };
+};
+export default connect(mapStateToProps, { navClick })(ButtonBox);
