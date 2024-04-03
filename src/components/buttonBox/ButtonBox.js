bade63935d7ba4c5156fea7a29756e347d61cf57
Steven <stevengf14@gmail.com>
2024-04-03 13:12:04 -0500

    initial view

---
+++ src/components/buttonBox/ButtonBox.js
@@ -0,0 +1,20 @@
+import "./ButtonBox.css";
+
+const ButtonBox = () => {
+  return (
+    <div className="ButtonBox">
+      <header className="App-header">
+        <div>
+          <h1>Title</h1>
+          <ul>
+            <li>Home</li>
+            <li>List</li>
+            <li>New</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default ButtonBox;
