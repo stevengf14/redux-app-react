c3f352fd765f90e002f2c7c06d7419fa820c18ab
Steven <stevengf14@gmail.com>
2024-04-05 16:22:44 -0500

    get the list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import { createBrowserRouter, Routes, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import ButtonBox from "./components/buttonBox/ButtonBox";
-import Home from "./components/home/Home";
-import List from "./components/list/List";
-import Form from "./components/form/Form";
-
-const router = createBrowserRouter([
-  { index: true, element: <Home /> },
-  { path: "list", element: <List /> },
-  { path: "new", element: <Form /> },
-]);
 
 function App() {
   return (
     <div className="App">
-      <ButtonBox />
-      <RouterProvider router={router}>
-        <Routes />
-      </RouterProvider>
+      <Router>
+        <Routes>
+          <Route path="/:section" component={ButtonBox} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/buttonBox/ButtonBox.js
+++ src/components/buttonBox/ButtonBox.js
@@ -2,32 +2,51 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import "./ButtonBox.css";
 import { navClick } from "../../redux/actions/navActions";
+import Home from "../home/Home";
+import List from "../list/List";
+import Form from "../form/Form";
 
-const ButtonBox = ({ title, navClick }) => {
+const ButtonBox = ({ title, navClick, match: { params } }) => {
   const buttonClick = (_title) => {
     navClick({
       title: _title,
     });
   };
+
+  const renderSection = () => {
+    switch (params.section) {
+      case "home":
+        return <Home />;
+      case "list":
+        return <List />;
+      case "new":
+        return <Form />;
+      default:
+        return;
+    }
+  };
+
   return (
     <div className="ButtonBox">
       <header className="App-header">
         <div>
           <h1>{title}</h1>
           <ul>
-            <li onClick={() => buttonClick("Home")}>Home</li>
-            <li onClick={() => buttonClick("List")}>List</li>
-            <li onClick={() => buttonClick("New")}>New</li>
+            <Link onClick={() => buttonClick("Home")}>Home</Link>
+            <Link onClick={() => buttonClick("List")}>List</Link>
+            <Link onClick={() => buttonClick("New")}>New</Link>
           </ul>
         </div>
       </header>
+      {renderSection()}
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
   return {
     title: state.navState.title,
+    section: ownProps.section,
   };
 };
 
