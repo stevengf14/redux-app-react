fc35998b3f62ae79dfea58438e90abf407027821
Steven <stevengf14@gmail.com>
2024-04-05 16:39:37 -0500

    get data init

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,7 +1,20 @@
 import { connect } from "react-redux";
 import "./List.css";
+import { useEffect } from "react";
+import axios from "axiosS";
 
 const List = () => {
+  useEffect(() => {
+    axios
+      .get("http://dev.contanimacion.com/api_tablon/api/mensajes")
+      .then(({ data }) => {
+        console.log("Data", data);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  }, []);
+
   return (
     <div className="List">
       <h1>List</h1>
