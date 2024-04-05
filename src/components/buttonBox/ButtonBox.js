1180387250dbdbea55c576bdaa6b1b2e26fdf00c
Steven <stevengf14@gmail.com>
2024-04-05 16:31:55 -0500

    react-router with Redux

--- src/components/buttonBox/ButtonBox.js
+++ src/components/buttonBox/ButtonBox.js
@@ -5,8 +5,29 @@ import { navClick } from "../../redux/actions/navActions";
 import Home from "../home/Home";
 import List from "../list/List";
 import Form from "../form/Form";
+import { useEffect } from "react";
 
 const ButtonBox = ({ title, navClick, match: { params } }) => {
+  useEffect(() => {
+    let title = "";
+    switch (params.section) {
+      case "home":
+        title = "Home";
+        break;
+      case "list":
+        title = "List";
+        break;
+      case "new":
+        title = "New";
+        break;
+      default:
+        title = "Home";
+    }
+    navClick({
+      title: title,
+    });
+  }, []);
+
   const buttonClick = (_title) => {
     navClick({
       title: _title,
