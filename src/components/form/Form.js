303e7ce26afe58313e3e4623845a3d8244d91fda
Steven <stevengf14@gmail.com>
2024-04-09 21:32:00 -0500

    form view

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,28 +1,60 @@
 import { connect } from "react-redux";
 import "./Form.css";
-import { reduxForm } from "redux-form";
+import { reduxForm, Field } from "redux-form";
 
-const validate = values => {
+const validate = (values) => {
   const errors = {};
 
   return errors;
-}
+};
+
+const renderField = ({ label, input, type }) => (
+  <div>
+    <label className="control-label">{label}</label>
+    <div>
+      <input
+        {...input}
+        type={type}
+        className="form-control"
+        placeholder="label"
+      />
+    </div>
+  </div>
+);
 
 let Form = ({}) => {
   return (
     <div className="Form">
-      <h1>New</h1>
+      <h3>Add new message</h3>
+      <form>
+        <div>
+          <Field
+            name="subject"
+            label="subject"
+            type="text"
+            component={renderField}
+          />
+        </div>
+      </form>
+      <div>
+        <Field
+          name="message"
+          label="message"
+          type="text"
+          component={renderField}
+        />
+      </div>
+      <div>
+        <input type="submit" value="Send" className="send" />
+      </div>
     </div>
   );
 };
 
-Form = reduxForm(
-  {
-    form: "new",
-    validate
-  }
-)
-(Form)
+Form = reduxForm({
+  form: "new",
+  validate,
+})(Form);
 const mapStateToProps = (state) => {
   return {};
 };
