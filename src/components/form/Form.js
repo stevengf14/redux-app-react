3d79a88f71f006339aee686a17167cf90fd8fa41
Steven <stevengf14@gmail.com>
2024-04-09 21:42:05 -0500

    form validation

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -4,11 +4,26 @@ import { reduxForm, Field } from "redux-form";
 
 const validate = (values) => {
   const errors = {};
+  if (!values.subject) {
+    errors.subject = "Required";
+  } else if (values.subject.length < 6) {
+    errors.subject = "Al least 2 more characters";
+  }
 
+  if (!values.message) {
+    errors.message = "Required";
+  } else if (values.message.length < 6) {
+    errors.message = "Al least 2 more characters";
+  }
   return errors;
 };
 
-const renderField = ({ label, input, type }) => (
+const renderField = ({
+  label,
+  input,
+  type,
+  meta: { touched, error, warning },
+}) => (
   <div>
     <label className="control-label">{label}</label>
     <div>
@@ -18,6 +33,9 @@ const renderField = ({ label, input, type }) => (
         className="form-control"
         placeholder="label"
       />
+      {touched &&
+        ((error && <span className="text-danger">{error}</span>) ||
+          (warning && <span className="text-warning">{warning}</span>))}
     </div>
   </div>
 );
